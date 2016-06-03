2e69e2f12f8894a6da150ec258d6b4444ef27489
Zachary Klein <zak@silver-chalice.com>
2016-06-03 11:42:04 -0500

    Initial commit of react profile

---
+++ skeleton/src/main/js/app/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zak on 5/3/16.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './hello';
+
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
---
+++ skeleton/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/main/js/index.js'
+    },
+    output: {
+        path: './grails-app/assets/javascripts',
+        publicPath: '/assets/',
+        filename: 'bundle.js'
+    },
+    loaders: [
+        {
+            test: /\.js$/,
+            include: path.join(__dirname, 'src/main/js'),
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+        {
+            test: /\.css$/,
+            loaders: ['style', 'css']
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loader: 'url?limit=10000&prefix=assets/!img'
+        }
+    ]
+};
+
