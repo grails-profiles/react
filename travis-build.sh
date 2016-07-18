c29f4e9ab850ab6abe198630b102e9b1f38486d1
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-18 17:30:20 +0200

    publish for master branch

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@ set -e
 ./gradlew clean 
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == '3.1.x' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[$TRAVIS_PULL_REQUEST == 'false' ]]; then
 
   echo "Publishing archives"
 
