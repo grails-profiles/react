5abf4d6650b31c17068742191c73c01acdf8f1f8
Zachary Klein <zak@silver-chalice.com>
2016-10-01 09:50:52 -0700

    Installed client via create-react-app

--- skeleton/client/src/main/webapp/app/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import About from './about';
-
-require('./../styles/style.css');
-
-ReactDOM.render(<About />, document.getElementById('app'));
