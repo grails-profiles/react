1492403fcadc8aeab14b3e76ef2d311457ea4636
Zachary Klein <zak@silver-chalice.com>
2016-10-31 22:25:08 -0700

    Removed dependency on bootstrap package, CSS fixes, added instructions

--- skeleton/client/src/index.js
+++ skeleton/client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
   <App />,
