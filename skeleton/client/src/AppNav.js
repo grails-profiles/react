91d7f2b60922f7a3eb5c6cedab02b4711da357f1
Zachary Klein <zak@silver-chalice.com>
2017-04-18 10:41:39 -0500

    Updated build.gradle to use Yarn tasks

--- skeleton/client/src/AppNav.js
+++ skeleton/client/src/AppNav.js
@@ -25,7 +25,6 @@ function AppNav({serverInfo, clientInfo}) {
             <MenuItem eventKey="4.2">Grails profile: {appprofile}</MenuItem>
             <MenuItem eventKey="4.2">Grails version: {grailsversion}</MenuItem>
             <MenuItem eventKey="4.3">React version: {clientInfo.react.replace('^', '')}</MenuItem>
-            <MenuItem divider />
             <MenuItem eventKey="4.3">Server version: {appversion}</MenuItem>
             <MenuItem eventKey="4.3">Client version: {clientInfo.version}</MenuItem>
             <MenuItem divider />
