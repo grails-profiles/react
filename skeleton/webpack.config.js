44426af0bcf0f9ac2f8cd446ded9124060cc94d2
Zachary Klein <zak@silver-chalice.com>
2017-05-04 12:53:33 -0500

    Upgraded to webpack 2

--- skeleton/webpack.config.js
+++ skeleton/webpack.config.js
@@ -1,33 +1,40 @@
 var path = require('path');
 
 module.exports = {
-    entry: {
-        index: './src/main/webapp/app/app.js'
-    },
-    output: {
-        path: './grails-app/assets/javascripts',
-        publicPath: '/assets/',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src/main/webapp'),
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css']
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: 'url?limit=10000&prefix=assets/!img'
-            }
+  entry: {
+    index: './src/main/webapp/app/app.js'
+  },
+  output: {
+    path: path.join(__dirname, 'grails-app/javascripts'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src/main/webapp'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["react", ["es2015", { "modules": false }]],
+            plugins: ["transform-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
         ]
-    }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'url?limit=10000&prefix=assets/!img'
+        }
+      }
+    ]
+  }
 };
-
