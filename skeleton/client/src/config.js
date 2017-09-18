6372e981f3404f410605510388d547abe9da30bc
sdelamo <sergio.delamo@softamo.com>
2017-09-18 11:11:34 +0200

    Remove files from branch docs

--- skeleton/client/src/config.js
+++
@@ -1,5 +0,0 @@
-import pjson from './../package.json';
-
-export const SERVER_URL = 'http://localhost:8080/';
-export const CLIENT_VERSION = pjson.version;
-export const REACT_VERSION = pjson.dependencies.react;
