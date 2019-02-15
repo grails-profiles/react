17f2cd5698e84cbf124a8ff7c1f643288e65bed7
Zachary Klein <zak@silver-chalice.com>
2019-02-14 23:48:21 -0600

    Updated to React 16.8

--- skeleton/client/src/App.js
+++ skeleton/client/src/App.js
@@ -10,10 +10,7 @@ import Footer from "./Footer";
 
 class App extends Component {
 
-  constructor() {
-    super();
-
-    this.state = {
+    state = {
       serverInfo: {},
       clientInfo: {
         version: CLIENT_VERSION,
@@ -21,62 +18,61 @@ class App extends Component {
       },
       collapse: false
     }
-  }
 
-  toggle = () => {
-    this.setState({collapse: !!this.state.collapse})
-  };
+    toggle = () => {
+        this.setState({collapse: !!this.state.collapse})
+    }
 
-  componentDidMount() {
-    fetch(SERVER_URL + '/application')
-      .then(r => r.json())
-      .then(json => this.setState({serverInfo: json}))
-      .catch(error => console.error('Error connecting to server: ' + error));
+    componentDidMount() {
+        fetch(SERVER_URL + '/application')
+            .then(r => r.json())
+            .then(json => this.setState({serverInfo: json}))
+            .catch(error => console.error('Error connecting to server: ' + error));
 
-  }
+    }
 
-  render() {
-    const {serverInfo, clientInfo, collapse} = this.state;
+    render() {
+        const {serverInfo, clientInfo, collapse} = this.state;
 
-    return [
-      <AppNav serverInfo={serverInfo} clientInfo={clientInfo} collapse={collapse} toggle={this.toggle} key={0}/>,
-      <div className="grails-logo-container" key={1}>
-        <img className="grails-logo" src={grailsLogo} alt="Grails"/>
-        <span className="plus-logo">+</span>
-        <img className="hero-logo" src={reactLogo} alt="React"/>
-      </div>,
+        return [
+            <AppNav serverInfo={serverInfo} clientInfo={clientInfo} collapse={collapse} toggle={this.toggle} key={0}/>,
+            <div className="grails-logo-container" key={1}>
+                <img className="grails-logo" src={grailsLogo} alt="Grails"/>
+                <span className="plus-logo">+</span>
+                <img className="hero-logo" src={reactLogo} alt="React"/>
+            </div>,
 
-      <Row key={2}>
-        <div id="content">
-          <section className="row colset-2-its">
-            <h1 style={{textAlign: 'center'}}>Welcome to Grails</h1>
-            <br/>
-            <p>
-              Congratulations, you have successfully started your Grails & React application! While in
-              development mode, changes will be loaded automatically when you edit your React app,
-              without even refreshing the page.
-              Below is a list of controllers that are currently deployed in
-              this application, click on each to execute its default action:
-            </p>
+            <Row key={2}>
+                <div id="content">
+                    <section className="row colset-2-its">
+                        <h1 style={{textAlign: 'center'}}>Welcome to Grails</h1>
+                        <br/>
+                        <p>
+                            Congratulations, you have successfully started your Grails & React application! While in
+                            development mode, changes will be loaded automatically when you edit your React app,
+                            without even refreshing the page.
+                            Below is a list of controllers that are currently deployed in
+                            this application, click on each to execute its default action:
+                        </p>
 
-            <div id="controllers" role="navigation">
-              <h2>Available Controllers:</h2>
-              <ul>
-                {serverInfo.controllers ? serverInfo.controllers.map(controller => {
-                  return <li key={controller.name}><a
-                    href={SERVER_URL + controller.logicalPropertyName}>{controller.name}</a>
-                  </li>;
-                }) : null}
-              </ul>
-            </div>
-          </section>
+                        <div id="controllers" role="navigation">
+                            <h2>Available Controllers:</h2>
+                            <ul>
+                                {serverInfo.controllers ? serverInfo.controllers.map(controller => {
+                                    return <li key={controller.name}><a
+                                        href={SERVER_URL + controller.logicalPropertyName}>{controller.name}</a>
+                                    </li>;
+                                }) : null}
+                            </ul>
+                        </div>
+                    </section>
 
-        </div>
+                </div>
 
-      </Row>,
-      <Footer key={3}/>
-    ];
-  }
+            </Row>,
+            <Footer key={3}/>
+        ];
+    }
 }
 
 export default App;
